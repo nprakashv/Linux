14f5c48bcd096744384b61f3f14352edeacabcc6
Nikhil Prakash V <vprakashnikhil@gmail.com>
2020-05-11 23:14:43 +0530

    kfifo: Adding sample driver to check kernel fifo.
    Signed-off-by: Nikhil Prakash V <vprakashnikhil@gmail.com>

---
+++ Drivers/kfifo.c
@@ -0,0 +1,54 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kfifo.h>
+#include <linux/kernel.h>
+#include <linux/gfp.h>
+#include <linux/errno.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+
+static struct kfifo kfifo;
+
+static int myinit(void)
+{
+	int ret;
+	char *ptr = "Hello Nikhil Prakash V";
+	char *buf;
+
+	ret = kfifo_alloc(&kfifo, 1000, GFP_KERNEL);
+	if (ret) {
+		pr_err("Fifo allocation failed. Memory not available\n");
+		return -ENOMEM;
+	}
+
+	ret = kfifo_in(&kfifo, ptr, strlen(ptr));
+	if (!ret) {
+		pr_err("Enqueue failed\n");
+		goto err_enq;
+	}
+
+	buf = (char *) kmalloc(strlen(ptr) + 1, GFP_KERNEL);
+
+	while(!kfifo_is_empty(&kfifo)) {
+		kfifo_out(&kfifo, buf, strlen(ptr));
+		buf[strlen(ptr)] = '\0';
+		pr_info("Buffer is %s\n", buf);
+	}
+
+	kfree(buf);
+	return 0;
+
+err_enq:
+	kfifo_free(&kfifo);
+	return -ENOSPC;
+}
+
+static void myexit(void)
+{
+	kfifo_free(&kfifo);
+}
+
+module_init(myinit);
+module_exit(myexit);
+
+MODULE_LICENSE("GPL");
